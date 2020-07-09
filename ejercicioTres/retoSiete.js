7dc52bb868e0801be753fa194651339576c5edec
Sara Colorado Gil <sara.coloradog@iefedericoozanam.edu.co>
2020-07-09 10:13:49 -0500

    Ejercicios terminados

---
+++ ejercicioTres/retoSiete.js
@@ -0,0 +1,4 @@
+
+const listElement= document.querySelector('.lista');
+const content= '<li>1 </li> <li>2 </li>  <li>3 </li> ';
+listElement.innerHTML =content; 
---
+++ ejercicioUno/retoDos.js
@@ -0,0 +1,2 @@
+const myAdress='Calle 46#34-62';
+myAdress='Calle 23 #45-65';
